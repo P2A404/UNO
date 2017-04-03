40f1a7096f3e88a54d01f214c630c19147bd44d0
Thomas Sørensen <tgsa16@student.aau.dk>
2017-04-03 12:00:22 +0200

    Indlæsning af filer
    Flytning af filer fra gammel repository.

---
+++ Uno/Uno/Card.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno
+{
+    class Card
+    {
+        public enum CardColor
+        {
+            Red, Blue, Green, Yellow, Black
+        }
+
+        public enum CardValue
+        {
+            Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Block, PlusTwo, PlusFour, Recolor, Reverse
+        }
+
+        CardColor Color;
+        CardValue Value;
+
+        public Card(CardColor color , CardValue value)
+        {
+            Color = color;
+            Value = value;
+        }
+
+
+
+    }
+}
