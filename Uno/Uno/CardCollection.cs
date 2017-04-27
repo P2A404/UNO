27b5821add43ad9b7866b83dd46fc19263f6c03d
Alex bak <abak16@student.aau.dk>
2017-04-27 10:07:09 +0200

    added deck constructor

--- Uno/Uno/CardCollection.cs
+++ Uno/Uno/CardCollection.cs
@@ -8,24 +8,48 @@ namespace Uno
 {
     abstract class CardCollection
     {
-        List<Card> cards = new List<Card>();
+        public List<Card> cards = new List<Card>();
     }
 
     class Deck : CardCollection
     {
         public Deck()
         {
-            
+            //add jokers and joker+4
+            for (int i = 0; i < 4; i++)
+            {
+                cards.Add(new Card(Card.CardColor.Black, Card.CardValue.Recolor));
+                cards.Add(new Card(Card.CardColor.Black, Card.CardValue.PlusFour));
+            }
+            //add 2 of each non-joker non-zero cards of each non-black color and 1 zero card of each non-black color
+            foreach (Card.CardColor CC in Enum.GetValues(typeof(Card.CardColor)))
+            {
+                foreach (Card.CardValue CV in Enum.GetValues(typeof(Card.CardValue)))
+                {
+                    if (CC != Card.CardColor.Black || CV != Card.CardValue.PlusFour || CV != Card.CardValue.Recolor)
+                    {
+                        if (CV == Card.CardValue.Zero)
+                        {
+                            cards.Add(new Card(CC, CV));
+                        }
+                        else
+                        {
+                            cards.Add(new Card(CC, CV));
+                            cards.Add(new Card(CC, CV));
+                        }
+                    }
+                }
+            }
         }
     }
 
     class Hand : CardCollection
     {
-        
+
     }
 
     class Pile : CardCollection
     {
-        
+
     }
 }
