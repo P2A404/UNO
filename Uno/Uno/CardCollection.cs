e8b5044339682631ef851d894f271f4972277673
MrLongbottom <perich16@student.aau.dk>
2017-04-27 09:41:03 +0200

    Made some CCs children

--- Uno/Uno/CardCollection.cs
+++ Uno/Uno/CardCollection.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Uno
 {
-    class CardCollection
+    abstract class CardCollection
     {
+        List<Card> cards = new List<Card>();
+    }
+
+    class Deck : CardCollection
+    {
+        public Deck()
+        {
+            
+        }
+    }
+
+    class Hand : CardCollection
+    {
+        
+    }
+
+    class Pile : CardCollection
+    {
+        
     }
 }
